93131091e8aebb4b3c5b388289cfb7543883c3d9
darshan16100896 <darshanmakwana0896@gmail.com>
2026-04-15 17:49:56 +0530

    fix: server error handling in the send email API

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -4,48 +4,56 @@ import ejs from "ejs";
 import { transporter } from "@/lib/mailer/transporter";
 
 export async function POST(req) {
-  const body = await req.json();
-  const ip =
-    req.headers.get("x-forwarded-for")?.split(",")[0]?.trim() ||
-    req.headers.get("x-real-ip") ||
-    req.headers.get("cf-connecting-ip") ||
-    "unknown";
+  try {
+    const body = await req.json();
 
-  if (!body.email || !body.subject || !body.message || !body.name) {
+    if (!body.email || !body.subject || !body.message || !body.name) {
+      return NextResponse.json(
+        { success: false, message: "Please fill out all the details." },
+        { status: 400 },
+      );
+    }
+    const templatePath = path.join(
+      process.cwd(),
+      "src",
+      "lib",
+      "email",
+      "templates",
+      `contact-form-email.ejs`,
+    );
+
+    const html = await ejs.renderFile(templatePath, {
+      fullName: body.name,
+      emailAddress: body.email,
+      subject: body.subject,
+      messageContent: body.message,
+    });
+
+    const mailOptions = {
+      from: '"Darshan Makwana\'s Portfolio" <darshanmakwana0896@gmail.com>',
+      to: process.env.CONTACT_FORM_RECIPIENT,
+      subject: "Website Contact Form Submission | Darshan Makwana's Portfolio",
+      html,
+    };
+
+    console.log({ mailOptions: { ...mailOptions, html: undefined, body } });
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json({
+      success: true,
+      message:
+        "Contact form submitted successfully! I'll get back to you soon.",
+    });
+  } catch (error) {
+    console.log("Error sending contact form email:", error);
     return NextResponse.json(
-      { success: false, message: "Please fill out all the details." },
-      { status: 400 },
+      {
+        success: false,
+        message:
+          "An error occurred while submitting the form. Please try again later.",
+      },
+      { status: 500 },
     );
   }
-  const templatePath = path.join(
-    process.cwd(),
-    "src",
-    "lib",
-    "email",
-    "templates",
-    `contact-form-email.ejs`,
-  );
-
-  const html = await ejs.renderFile(templatePath, {
-    fullName: body.name,
-    emailAddress: body.email,
-    subject: body.subject,
-    messageContent: body.message,
-  });
-
-  const mailOptions = {
-    from: '"Darshan Makwana\'s Portfolio" <darshanmakwana0896@gmail.com>',
-    to: process.env.CONTACT_FORM_RECIPIENT,
-    subject: "Website Contact Form Submission | Darshan Makwana's Portfolio",
-    html,
-  };
-
-  console.log({ mailOptions: { ...mailOptions, html: undefined, body } });
-
-  await transporter.sendMail(mailOptions);
-
-  return NextResponse.json({
-    success: true,
-    message: "Contact form submitted successfully! I'll get back to you soon.",
-  });
 }
